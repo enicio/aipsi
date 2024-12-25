0ae0275cbdfef7bfbf14e0905ce7590e2d236107
enicio <jessandro42@gmail.com>
2024-12-25 02:41:31 -0300

    Add service layer to business module

---
+++ src/modules/business/services/enterprise.service.ts
@@ -0,0 +1,26 @@
+import type { DataSource, Repository } from 'typeorm';
+import { Enterprise } from '../entities/enterprise.entity';
+
+export class EnterpriseService {
+  private repository: Repository<Enterprise>;
+
+  constructor(dataSource: DataSource) {
+    this.repository = dataSource.getRepository(Enterprise);
+  }
+
+  async create(data: Partial<Enterprise>): Promise<Enterprise> {
+    const enterprise = this.repository.create(data);
+    return this.repository.save(enterprise);
+  }
+
+  async findAll(): Promise<Enterprise[]> {
+    return this.repository.find();
+  }
+
+  async findById(id: string): Promise<Enterprise | null> {
+    return this.repository.findOne({
+      where: { id },
+      relations: ['employees', 'devices']
+    });
+  }
+}
